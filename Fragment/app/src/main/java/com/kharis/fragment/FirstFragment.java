65908438866694c7e6e278246fc4a7e761199428
KharisMm19 <kharismm19@gmail.com>
2019-07-03 10:46:52 +0700

    tambah fragment

---
+++ Fragment/app/src/main/java/com/kharis/fragment/FirstFragment.java
@@ -0,0 +1,32 @@
+package com.kharis.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstFragment extends Fragment {
+    View view;
+    Button firstButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+        firstButton = view.findViewById(R.id.btn_firstFragment);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"First Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+
+}
--- Fragment/app/src/main/java/com/kharis/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/kharis/fragment/MainActivity.java
@@ -1,14 +1,44 @@
 package com.kharis.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_firstFrag, btn_secondFrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_firstFrag = findViewById(R.id.btn_firstFragment);
+        btn_secondFrag = findViewById(R.id.btn_secondFragment);
+
+        btn_firstFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        btn_secondFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.frameLayout,fragment);
+        ft.commit();
     }
 }
