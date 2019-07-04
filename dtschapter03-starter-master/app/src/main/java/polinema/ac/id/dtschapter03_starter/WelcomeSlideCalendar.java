9a7e5579b19f70f04e4b7eff2eb4175937dd4713
KharisMm19 <kharismm19@gmail.com>
2019-07-04 11:43:17 +0700

    poltek padam

--- dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideCalendar.java
+++ dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideCalendar.java
@@ -1,7 +1,9 @@
 package polinema.ac.id.dtschapter03_starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeSlideCalendar extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class WelcomeSlideCalendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_slide_calendar);
+    }
+
+    public void clickGetStarted(View view) {
+        Intent i = new Intent(WelcomeSlideCalendar.this, welcomeSlideSuperHero.class);
+        startActivity(i);
+    }
+
+    public void clickLogin(View view) {
+        Intent i = new Intent(WelcomeSlideCalendar.this, WelcomeBack.class);
+        startActivity(i);
     }
 }
