4f52b7a9be9d8664a2b604c1aff2377e9677846e
KharisMm19 <kharismm19@gmail.com>
2019-07-04 12:18:34 +0700

    haha

--- dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/welcomeSlideSuperHero.java
+++ dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/welcomeSlideSuperHero.java
@@ -1,7 +1,9 @@
 package polinema.ac.id.dtschapter03_starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class welcomeSlideSuperHero extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class welcomeSlideSuperHero extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_slide_super_hero);
+    }
+
+    public void clickGetStarted(View view) {
+        Intent i = new Intent(welcomeSlideSuperHero.this, WelcomeSlideAssign.class);
+        startActivity(i);
+    }
+
+    public void clickLogin(View view) {
+        Intent i = new Intent(welcomeSlideSuperHero.this, WelcomeBack.class);
+        startActivity(i);
     }
 }
