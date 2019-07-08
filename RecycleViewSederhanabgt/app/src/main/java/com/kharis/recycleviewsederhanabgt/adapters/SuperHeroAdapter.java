690060dfedfc79d0056669e7f33cc2cc37edb505
KharisMm19 <kharismm19@gmail.com>
2019-07-08 09:49:34 +0700

    Hero adapter

---
+++ RecycleViewSederhanabgt/app/src/main/java/com/kharis/recycleviewsederhanabgt/adapters/SuperHeroAdapter.java
@@ -0,0 +1,46 @@
+package com.kharis.recycleviewsederhanabgt.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kharis.recycleviewsederhanabgt.R;
+import com.kharis.recycleviewsederhanabgt.models.SuperHero;
+
+import java.util.List;
+
+public class SuperHeroAdapter extends RecyclerView.Adapter<SuperHeroAdapter.MyViewHolder> {
+    List<SuperHero> heroList;
+
+    @NonNull
+    @Override
+    public SuperHeroAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View superHeroView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_super_hero,parent,false);
+        return new MyViewHolder(superHeroView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuperHeroAdapter.MyViewHolder holder, int position) {
+        SuperHero superHero = heroList.get(position);
+        holder.heroName.setText(superHero.getHeroName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return heroList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView heroName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            heroName = itemView.findViewById(R.id.heroName);
+        }
+    }
+}
--- RecycleViewSederhanabgt/app/src/main/java/com/kharis/recycleviewsederhanabgt/models/SuperHero.java
+++ RecycleViewSederhanabgt/app/src/main/java/com/kharis/recycleviewsederhanabgt/models/SuperHero.java
@@ -1,4 +1,17 @@
 package com.kharis.recycleviewsederhanabgt.models;
 
 public class SuperHero {
+    public String heroName;
+
+    public SuperHero(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
 }
