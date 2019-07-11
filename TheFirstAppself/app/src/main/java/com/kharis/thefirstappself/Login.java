9295697834d745199185b4ae40487329b3c862fd
KharisMm19 <kharismm19@gmail.com>
2019-07-11 12:05:37 +0700

    huhh

--- TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/Login.java
+++ TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/Login.java
@@ -4,19 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.edt_username);
+        editTextPassword = findViewById(R.id.edt_password);
+
     }
 
-    public void clickLogin(View view) {
-        Intent i = new Intent(Login.this, HomeScreen.class);
-        startActivity(i);
+    public void postLogin(View view) {
+        // Validasi input email dan password kosong
+        if(TextUtils.isEmpty(editTextEmail.getText().toString().trim()) &&
+                TextUtils.isEmpty(editTextPassword.getText().toString().trim())) {
+            Toast.makeText(view.getContext(), "Email dan Password tidak boleh kosong!",
+                    Toast.LENGTH_LONG).show();
+        }
+        // Validasi input email kosong
+        if(TextUtils.isEmpty(editTextEmail.getText().toString().trim())){
+            Toast.makeText(view.getContext(), "Email tidak boleh kosong!", Toast.LENGTH_LONG).show();
+        }
+        // Validasi inputan tipe email
+        else if(!isValidEmail(editTextEmail.getText().toString().trim())) {
+            Toast.makeText(view.getContext(), "Email tidak valid!", Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(editTextPassword.getText().toString())) {
+            Toast.makeText(view.getContext(), "Password tidak boleh kosong!", Toast.LENGTH_LONG).show();
+        }
+        // Validasi inputan password tidak boleh kosong
+        else {
+            Intent i = new Intent(Login.this, HomeScreen.class);
+            startActivity(i);
+        }
+    }
+
+    public static boolean isValidEmail(CharSequence email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
     public void clickSignUp(View view) {
--- TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/SignUp.java
+++ TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/SignUp.java
@@ -5,17 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    private final String TAG = SignUp.class.getName();
+    public static final String Key_SignUp = "Key_SignUp";
+
+    EditText newTextEmail;
+    EditText newTextPassword;
+    EditText name;
+    EditText tanggalLahir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name = findViewById(R.id.edt_firstname);
+        tanggalLahir = findViewById(R.id.edt_tanggal_lahir);
+        newTextEmail = findViewById(R.id.edt_username);
+        newTextPassword = findViewById(R.id.edt_password);
     }
 
-    public void clickSignUp(View view) {
+    public void postSignUp(View view) {
+        String pass = newTextPassword.getText().toString();
+        String username = newTextEmail.getText().toString();
+        String nama = name.getText().toString();
+        String tangga_lahir = tanggalLahir.getText().toString();
         Intent i = new Intent(SignUp.this, Login.class);
+        i.putExtra(Key_SignUp, new String[]{nama, tangga_lahir, username});
         startActivity(i);
     }
 
