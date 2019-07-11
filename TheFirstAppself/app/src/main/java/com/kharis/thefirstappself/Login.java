a0fc814431fd2014ee20ca9ecc9f123af97391d0
KharisMm19 <kharismm19@gmail.com>
2019-07-11 10:35:07 +0700

    hadeuhh

--- TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/Login.java
+++ TheFirstAppself/app/src/main/java/com/kharis/thefirstappself/Login.java
@@ -2,7 +2,9 @@ package com.kharis.thefirstappself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickLogin(View view) {
+        Intent i = new Intent(Login.this, HomeScreen.class);
+        startActivity(i);
+    }
+
+    public void clickSignUp(View view) {
+        Intent i = new Intent(Login.this, SignUp.class);
+        startActivity(i);
     }
 }
