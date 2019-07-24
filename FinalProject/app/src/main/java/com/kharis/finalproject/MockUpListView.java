6150c0b343d97d3ee76529df7d4cc1ae738ce9e9
KharisMm19 <kharismm19@gmail.com>
2019-07-24 11:16:44 +0700

    listView udah donggggg......

--- FinalProject/app/src/main/java/com/kharis/finalproject/MockUpListView.java
+++ FinalProject/app/src/main/java/com/kharis/finalproject/MockUpListView.java
@@ -5,13 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MockUpListView extends AppCompatActivity {
+
+    private ListView lvItem;
+    private String[] namaNegara = new String[]{
+            "Indonesia", "Malaysia", "Singapore", "Spanyol", "Jepang",
+            "Inggris", "Belanda", "Mesir", "Brazil", "Italia", "Prancis",
+            "Thailand"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock_up_list_view);
+
+        getSupportActionBar().setTitle("ListView Sederhana");
+
+        lvItem = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MockUpListView.this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, namaNegara);
+
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MockUpListView.this,"Memilih : "
+                        +namaNegara[position],Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void clickNama(View view) {
